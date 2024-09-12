168de88884caf70f4bcda93fbf3f5b0e97d4f3b5
moshin-gyagenda <1900727591@mubs.ac.ug>
2024-09-12 12:01:13 +0300

    first commit

---
+++ src/myapp/Myapp.java
@@ -0,0 +1,14 @@
+package myapp;
+import java.util.*;
+public class Myapp {
+
+	public static void main(String[] args) {
+		
+	Scanner input = new Scanner(System.in);
+	System.out.println("Enter Your Name");
+	String name = input.nextLine();
+	System.out.println(name);
+
+	}
+
+}
